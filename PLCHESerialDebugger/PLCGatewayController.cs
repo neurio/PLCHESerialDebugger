578aaf8da146cd3a6dbfa0d1e26bb856cf6afa7c
mikeb20 <mike.beck@generac.com>
2025-01-02 17:32:52 -0800

    updated nuget package version

--- PLCHESerialDebugger/PLCGatewayController.cs
+++ PLCHESerialDebugger/PLCGatewayController.cs
@@ -136,7 +136,7 @@ namespace PLCHESerialDebugger
                         }
                     case PLCGatewayType.SerialPLCHE:
                         {
-                            PLCGateway = new SerialPLCHE(comPortNumber: SerialPortNumber, baudRate: SerialBaudRate);
+                            PLCGateway = new SerialPLCHE(ComPortNumber: SerialPortNumber, BaudRate: SerialBaudRate);
                             break;
                         }
                 }
